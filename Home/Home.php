f0ca84caf4638d8d9464553c3c4910bf3a20c510
Jakub Wilk <jakubwilk@htl-wels.at>
2022-06-03 17:40:13 +0200

    35% der Hauptseite gemacht

--- Home/Home.php
+++ Home/Home.php
@@ -7,8 +7,8 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script>
         $(document).ready(function() {
+          $("#h1").delay(500);
           $("#h1").fadeIn(3000);
-
         });
         </script>
     </head>
@@ -32,24 +32,25 @@
                 <section class="homebackground">
                     <img src="../Bilder/Homebilder/Wolf.jpg" width="100%" height="100%">
                 </section>
+
                 <div class = "sec1">
-                  <h3 id="h3"> in der Zukunft <br>
-                              mit Wilk-Industries</h3>
+                  <h3 id="h3"> in der Zukunft <br> mit Wilk-Industries <hr class = "hr1"> </h3>
+
+
+
                   <div class = "inhalt">
-                    <h2>Tägliche News</h2>
+                    <h2 id = "h2sec1">Tägliche News</h2>
+                    <br>
                     <p>
                       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
-                      Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-
-                      Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
-
-                      Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer
                     </p>
                   </div>
                 </div>
+
+
           </article>
           <footer>
+
           </footer>
       </body>
 </html>
