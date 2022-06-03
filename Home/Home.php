eb222b5f1126abeeb62e4eee063d7d143e53414b
Jakub Wilk <jakubwilk@htl-wels.at>
2022-06-03 18:19:20 +0200

    1.Zwischenstand

---
+++ Home/Home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+      <meta charset="utf-8">
+      <link rel = "stylesheet" href = "Home.css">
+      <title>Home</title>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script>
+        $(document).ready(function() {
+          $("#h1").delay(500);
+          $("#h1").fadeIn(3000);
+        });
+        </script>
+    </head>
+      <body>
+          <header>
+              <div class = "container">
+                <div class = "navbar">
+                  <nav>
+                    <ul>
+                      <li><a href="">Home</a></li>
+                      <li><a href="">Unternehmen</a></li>
+                      <li><a href="">Kontakt</a></li>
+                      <li><a href="">Produkte</a></li>
+                    </ul>
+                  </nav>
+                </div>an
+              </div>
+          </header>
+          <article>
+            <h1 id="h1"> Willkommen</h1>
+                <section class="homebackground">
+                    <img src="../Bilder/Homebilder/Wolf.jpg" width="100%" height="100%">
+                </section>
+
+                <div class = "sec1">
+                  <h3 id="h3"> in der Zukunft <br> mit Wilk-Industries <hr class = "hr1"> </h3>
+
+
+
+                  <div class = "inhalt">
+                    <h2 id = "h2sec1">Tägliche News</h2>
+                    <br>
+                    <p>
+                      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    </p>
+                  </div>
+                </div>
+
+
+          </article>
+          <footer>
+
+          </footer>
+      </body>
+</html>
